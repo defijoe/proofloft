41e716fd16bd80686ca1eb53d7ff7186d61cfd04
trendinux <trendinux@gmail.com>
2026-08-19 14:56:52 -0400

    Export createBillingPortalUrl

--- src/core/index.ts
+++ src/core/index.ts
@@ -8,7 +8,7 @@ export {
   sessionCookieOptions,
   type SessionUser,
 } from "./auth";
-export { createCheckoutUrl, verifyWebhookSignature, handleWebhook, isPro, getPlan, type Plan } from "./billing";
+export { createCheckoutUrl, createBillingPortalUrl, verifyWebhookSignature, handleWebhook, isPro, getPlan, type Plan } from "./billing";
 export { sendEmail } from "./email";
 export { brandedEmail, escapeHtml, starsHtml } from "./email-templates";
 export { track } from "./events";
